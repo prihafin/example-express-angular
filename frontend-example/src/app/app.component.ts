f8d1669dca243f4772cddd7583f56954a2e8ccba
prihafin <prihafin@gmail.com>
2021-05-13 21:16:44 +0300

    use proxy, removes need for config file
    removes the need to use custom configuration, use documented http server proxy features instead in production

--- frontend-example/src/app/app.component.ts
+++ frontend-example/src/app/app.component.ts
@@ -1,12 +1,5 @@
 import { Component } from '@angular/core';
 
-let config: any = undefined;
-
-async function getCongig() {
-  if (!config) config = await (await fetch('/assets/config.json')).json();
-  return config;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -20,8 +13,6 @@ export class AppComponent {
   async ngOnInit() {}
 
   async clickButton() {
-    let cfg = await getCongig();
-    console.log(cfg);
-    this.things = await (await fetch(cfg.api + '/things')).json();
+    this.things = await (await fetch('/api/things')).json();
   }
 }
